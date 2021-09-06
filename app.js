b6b1d7d1b71ac373c3a2c83a4a648690eaf9025f
sireesha2271 <sireeshag333@gmail.com>
2021-09-06 19:05:17 +0530

    bg changes

--- app.js
+++ app.js
@@ -16,8 +16,8 @@
         .then(response =>{
             console.log(response);
             for(var i = 0; i<response.items.length; i++){
-                document.getElementById('result').innerHTML+='<h4>'+response.items[i].volumeInfo.title+'</h4>'+
-                '<h5>By: '+response.items[i].volumeInfo.authors+'</h5><br><img class = "card z-depth-10" id="dynamic" src =' + response.items[i].volumeInfo.imageLinks.thumbnail + '><br><a href =' + response.items[i].volumeInfo.infoLink + '><button class="btn black">Read More</button></a>';
+                document.getElementById('result').innerHTML+='<h4 class="white-text">'+response.items[i].volumeInfo.title+'</h4>'+
+                '<h5 class="white-text">By: '+response.items[i].volumeInfo.authors+'</h5><br><img class = "card z-depth-10" id="dynamic" src =' + response.items[i].volumeInfo.imageLinks.thumbnail + '><br><a href =' + response.items[i].volumeInfo.infoLink + '><button class="btn blue" id="imagebutton">Read More</button></a>';
             }
         });
         
